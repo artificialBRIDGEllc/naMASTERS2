03ad4c0e6e9408933679ac3c9ba07fe7af83bdf1
Michael Lang <mlang@team-iia.com>
2026-04-14 20:05:19 -0500

    Add main application structure with routing and queries

---
+++ client/src/app.tsx
@@ -0,0 +1,31 @@
+import { Switch, Route } from "wouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SimulatorPage from "@/pages/simulator";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={SimulatorPage} />
+      <Route path="/simulator" component={SimulatorPage} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
